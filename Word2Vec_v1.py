fe581cd0d9f62d36dca34e57d2eb5da7f4ca201d
dhr1676 <ding.h@northeastern.edu>
2018-05-09 13:18:51 +0800

    Initial commit

---
+++ Word2Vec_v1.py
@@ -0,0 +1,47 @@
+import re
+# import time
+# import logging
+
+import pandas as pd
+import numpy as np
+
+from bs4 import BeautifulSoup
+
+import nltk
+# nltk.download()  # Download text data sets, including stop words
+from nltk.corpus import stopwords
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+
+train = pd.read_csv("labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+test = pd.read_csv("testData.tsv", header=0, delimiter="\t", quoting=3)
+unlabeled_train = pd.read_csv("unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+
+
+# Verify the number of reviews that were read (100,000 in total)
+# print("Read %d labeled train reviews, %d labeled test reviews, "
+#       "and %d unlabeled reviews\n" % (train["review"].size,
+#                                       test["review"].size,
+#                                       unlabeled_train["review"].size))
+
+def review_to_wordlist(review, remove_stopwords=False):
+    # Function to convert a document to a sequence of words,
+    # optionally removing stop words.  Returns a list of words.
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(review).get_text()
+    #
+    # 2. Remove non-letters
+    review_text = re.sub("[^a-zA-Z]", " ", review_text)
+    #
+    # 3. Convert words to lower case and split them
+    words = review_text.lower().split()
+    #
+    # 4. Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #
+    # 5. Return a list of words
+    return words
